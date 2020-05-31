a1da09c3910e1630baf57b3c4e5808a0a13d7838
Olabisi <ajoseholabisi@gmail.com>
2020-05-31 19:23:45 +0100

    for the animation page

--- routes/index.js
+++ routes/index.js
@@ -228,6 +228,10 @@ router.get("/logout", function(req,res){
 router.get("/about", function(req, res){
     res.render("about");
 })
+
+router.get("/animation", function(req, res){
+    res.render("animation");
+})
 // //middleware
 // function isLoggedIn(req, res, next){
 //     if(req.isAuthenticated()){
