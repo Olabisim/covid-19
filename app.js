28106300ce8093c0b89a2d44a793666238e7774b
Olabisi <ajoseholabisi@gmail.com>
2020-06-16 23:15:16 +0100

    completing the test

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ var express = require("express"),
     Like = require("./models/like"),
     middleware = require("./middleware"),
     cheerio = require('cheerio');
- 
 
+
+const nodemailer = require('nodemailer');
 
 //img
 //https://images.unsplash.com/photo-1586521995568-39abaa0c2311?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80
@@ -248,6 +249,98 @@ app.post("/likes", middleware.isLoggedIn, function(req, res){
                         })
                 }
         })
+})
+
+
+
+
+app.post('/contactfa', (req,res) => {
+        "use strict";
+
+        // async..await is not allowed in global scope, must use a wrapper
+        async function main() {
+        // Generate test SMTP service account from ethereal.email
+        // Only needed if you don't have a real mail account for testing
+        let testAccount = await nodemailer.createTestAccount();
+
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+                service: 'Gmail',
+                auth: {
+                        user: 'ajoseholabisi@gmail.com', // generated ethereal user
+                        pass: 'Olabisiminasu090', // generated ethereal password
+                },
+        });
+
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+                from: 'ajoseholabisi@gmail.com', // sender address
+                to: "ajoseolabisiii@gmail.com", // list of receivers
+                subject: "COVID-19", // Subject line
+                text: req.body.bodyy,
+                html: '<p style="color: green;">✔ A nigerian just checked</p><h4 style="color: black;"> ' + req.body.bodyy + ' </h4><p style="color: black;">tested by </p><p> <a href="https://pacific-hollows-29220.herokuapp.com/ctest"> https://pacific-hollows-29220.herokuapp.com/ctest </a></p>'
+        });
+
+        
+        console.log(req.body.user_name);
+        console.log(req.body.user_mail);
+        console.log(req.body.user_message);
+        
+        console.log("Message sent: %s", info.messageId);
+                // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+                // Preview only available when sending through an Ethereal account
+                console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+                // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        }
+
+        main().catch(console.error);
+        res.redirect("/");
+})
+
+
+app.post('/contacty', (req,res) => {
+        "use strict";
+
+        // async..await is not allowed in global scope, must use a wrapper
+        async function main() {
+        // Generate test SMTP service account from ethereal.email
+        // Only needed if you don't have a real mail account for testing
+        let testAccount = await nodemailer.createTestAccount();
+
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+                service: 'Gmail',
+                auth: {
+                        user: 'ajoseholabisi@gmail.com', // generated ethereal user
+                        pass: 'Olabisiminasu090', // generated ethereal password
+                },
+        });
+
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+                from: 'ajoseholabisi@gmail.com', // sender address
+                to: "ajoseolabisiii@gmail.com", // list of receivers
+                subject: "COVID-19", // Subject line
+                text: req.body.bodyy,
+                html: '<p style="color: green;">✔ A nigerian just checked</p><p>name: ' + req.body.name + ' </p><p>phone no.: ' + req.body.number + ' </p><h4 style="color: black;"> ' + req.body.bodyy + ' </h4><p style="color: black;">tested by </p><p> <a href="https://pacific-hollows-29220.herokuapp.com/ctest"> https://pacific-hollows-29220.herokuapp.com/ctest </a></p>'
+        });
+
+        
+        console.log(req.body.user_name);
+        console.log(req.body.user_mail);
+        console.log(req.body.user_message);
+        
+        console.log("Message sent: %s", info.messageId);
+                // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+                // Preview only available when sending through an Ethereal account
+                console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+                // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        }
+
+        main().catch(console.error);
+        res.redirect("/");
 })
 
 
